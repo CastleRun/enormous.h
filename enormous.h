d16e83a514a0c8aa487e9d725e87d7cd27867b99
CastleRun <786901a@gmail.com>
2016-12-03 21:56:11 -0800

    Update enormous.h

--- enormous.h
+++ enormous.h
@@ -5,21 +5,29 @@ Pre-Alpha
 
 #include <limits.h>
 #include <math.h>
-#define HALF ((int) (pow((2), (16)) - (1)))
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
+#define HALF ((unsigned int) (pow((2), (16)) - (1)))
 
 typedef enum { false, true } bool;
 typedef struct{
-  int i;
+  unsigned int i;
   bool c;
 } iwc;
+typedef struct{
+   unsigned int i;
+   unsigned int c;
+} ipwc;
 typedef struct{
   unsigned int i[16];
 } enormous;
 
+unsigned short splitInt(unsigned int n, bool high){
+   unsigned short s = n % (HALF + 1);
+   if(high){
+      return n - (unsigned int) s;
+   }else{
+      return s;
+   }
+}
 iwc awc(unsigned int a, unsigned int b, bool ci){
   iwc c;
   c.c = 0;
@@ -42,6 +50,21 @@ iwc awc(unsigned int a, unsigned int b, bool ci){
   }
   return c;
 }
+iwc itoiwc(unsigned int i){
+   iwc a;
+   a.i = i;
+   a.c = 0;
+   return a;
+}
 enormous add(enormous a, enourmous b){
-  //Not finished
+  enormous c;
+  iwc d;
+  bool ci = 0
+  int i;
+  for(i = 0; i <16){
+     d = awc(itoiwc(a),itoiwc(b));
+     c.i[i] = d.i;
+     ci = d.c;
+  }
+  return c;
 }
